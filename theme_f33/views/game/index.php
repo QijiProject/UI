bdc93a41b39e7ddb891b1019d246f932392690e7
WeiLun93 <wl.kwan@lokchong.com>
2017-03-02 09:11:42 +0800

    02Mar17 updates

--- theme_f33/views/game/index.php
+++ theme_f33/views/game/index.php
@@ -43,13 +43,13 @@ if($this->is_login()){
             <div class="games-platform-item mg-item ">
                 <ul class="games-sub-menu clearfix">
                     <?= $this->renderPartial('/game/mgMenu') ?>
+                    <li class="tab11 games-mg-search" style="display:none;"><span>搜索结果</span></li>
                 </ul>
-                <div class="game-search">
-                    <input type="text" placeholder="MG游戏搜索" />
-                </div>
+                <div class="search-wrap clearfix"><input type="text" id="mg-keyword" placeholder="MG游戏搜索" /><a onclick="mg_search();" href="javascript:void(0);"></a></div>
                 <div id="mggame_list">
                     <div class="games-item clearfix">
                         <?= $this->renderPartial('/game/mgGameList') ?>
+                        <ul id="mg-search-result" class="clearfix hide"></ul>
                     </div>
                 </div>
             </div>
@@ -59,13 +59,13 @@ if($this->is_login()){
                 <a class="pt-pc-download" id="pt_download_start" href="javascript:void(0);">PT客户端下载</a>
                 <ul class="games-sub-menu clearfix">
                     <?= $this->renderPartial('/game/ptMenu') ?>
+                    <li class="tab7 games-pt-search" style="display:none;"><span>搜索结果</span></li>
                 </ul>
-                <div class="game-search">
-                    <input type="text" placeholder="PT游戏搜索" />
-                </div>
+                <div class="search-wrap clearfix"><input type="text" id="pt-keyword" placeholder="PT游戏搜索" /><a href="javascript:void(0);" onclick="pt_search();"></a></div>
                 <div id="ptgame_list">
                     <div class="games-item clearfix">
                         <?= $this->renderPartial('/game/ptGameList') ?>
+                        <ul id="pt-search-result" class="clearfix hide"></ul>
                     </div>
                 </div>
             </div>
@@ -74,13 +74,13 @@ if($this->is_login()){
             <div class="games-platform-item bb-item hide">
                 <ul class="games-sub-menu clearfix">
                     <?= $this->renderPartial('/game/bbMenu') ?>
+                    <li class="tab9 games-bb-search" style="display:none;"><span>搜索结果</span></li>
                 </ul>
-                <div class="game-search">
-                    <input type="text" placeholder="BB游戏搜索" />
-                </div>
+                <div class="search-wrap clearfix"><input type="text" id="bb-keyword" placeholder="BBIN游戏搜索" /><a href="javascript:void(0);" onclick="bb_search();"></a></div>
                 <div id="bbgame_list">
                     <div class="games-item clearfix">
                         <?= $this->renderPartial('/game/bbGameList') ?>
+                        <ul id="bb-search-result" class="clearfix hide"></ul>
                     </div>
                 </div>
             </div>
@@ -98,13 +98,13 @@ if($this->is_login()){
             <div class="games-platform-item nt-item hide">
                 <ul class="games-sub-menu clearfix">
                     <?= $this->renderPartial('/game/ntMenu') ?>
+                    <li class="tab4 games-nt-search" style="display:none;"><span>搜索结果</span></li>
                 </ul>
-                <div class="game-search">
-                    <input type="text" placeholder="NT游戏搜索" />
-                </div>
+                <div class="search-wrap clearfix"><input type="text" id="nt-keyword" placeholder="NT游戏搜索" /><a href="javascript:void(0);" onclick="nt_search();"></a></div>
                 <div id="ntgame_list">
                     <div class="games-item clearfix">
                         <?= $this->renderPartial('/game/ntGameList',array('nt_status'=>$nt_status)) ?>
+                        <ul id="nt-search-result" class="clearfix hide"></ul>
                     </div>
                 </div>
             </div>
@@ -123,6 +123,19 @@ $(document).ready(function(){
     });
 
     $(".games-sub-menu").find("li").click(function(){
+        if(!$(this).hasClass("games-pt-search")){
+            $(".games-pt-search").hide();
+        }
+        if(!$(this).hasClass("games-mg-search")){
+            $(".games-mg-search").hide();
+        }
+        if(!$(this).hasClass("games-bb-search")){
+            $(".games-bb-search").hide();
+        }
+        if(!$(this).hasClass("games-nt-search")){
+            $(".games-nt-search").hide();
+        }
+        
         $(".games-sub-menu").find(".current").removeClass("current");
         var index = $(this).attr("class").replace("tab",'');
         index = parseInt(index)-1;
@@ -146,7 +159,26 @@ $(document).ready(function(){
     $('#bbgame_list li').bind('click',function(){
        var gameId = $(this).find("em").attr("class").replace('Game_','');
        load_bbin_game(<?=$bbin_status?>,'<?=BBIN::CH ?>',gameId);
-
+    });
+    
+    $('#pt-keyword').keypress(function (e) {
+        var key = e.which;
+        if (key == 13) {pt_search();} 
+    });
+    
+    $('#mg-keyword').keypress(function (e) {
+        var key = e.which;
+        if (key == 13) {mg_search();} 
+    });
+    
+    $('#bb-keyword').keypress(function (e) {
+        var key = e.which;
+        if (key == 13) {bb_search();} 
+    });
+    
+    $('#nt-keyword').keypress(function (e) {
+        var key = e.which;
+        if (key == 13) {nt_search();} 
     });
     
     //电子游戏导航下拉菜单点击选择对应平台
@@ -155,6 +187,79 @@ $(document).ready(function(){
     var oo = $("."+ml+"-menu");
     if(oo.length>0){oo.click();}
 });
+
+//pt-游戏搜索
+function pt_search(){
+    var b = [];
+    var k = $("#pt-keyword").val();
+    if(k=="")return;
+    $(".games-pt-search").show().click();
+    $("#pt-search-result").show().html('');
+    $(".pt-item .game-name").each(function(){
+        if($(this).html().indexOf(k)!=-1){
+            $("#pt-search-result").append('<li>'+$(this).parent().html()+'</li>');
+        }
+    });
+    $('#pt-search-result li').bind('click',function(){
+       var gameId = $(this).find("em").attr("class").replace('game_','');
+        load_pt('<?=PT::PT_RE ?>',<?=$pt_status ?>,gameId);
+    });
+}
+
+//mg-游戏搜索
+function mg_search(){
+    var b = [];
+    var k = $("#mg-keyword").val();
+    if(k=="")return;
+    $(".games-mg-search").show().click();
+    $("#mg-search-result").html('');
+    $(".mg-item .game-name").each(function(i){
+        if($(this).html().indexOf(k)!=-1){
+             console.log($(this).parent().html());
+            $("#mg-search-result").append('<li>'+$(this).parent().html()+'</li>');
+        }
+    });
+    $('#mg-search-result li').bind('click',function(){
+        var gameId = $(this).find("em").attr("gameid");
+        load_mg(<?=$mg_status?>,gameId);
+    });
+}
+
+//bb-游戏搜索
+function bb_search(){
+    var b = [];
+    var k = $("#bb-keyword").val();
+    if(k=="")return;
+    $(".games-bb-search").show().click();
+    $("#bb-search-result").html('');
+    $(".bb-item .game-name").each(function(){
+        if($(this).html().indexOf(k)!=-1){
+            $("#bb-search-result").append('<li>'+$(this).parent().html()+'</li>');
+        }
+    });
+    $('#bb-search-result li').bind('click',function(){
+       var gameId = $(this).find("em").attr("class").replace('Game_','');
+       load_bbin_game(<?=$bbin_status?>,'<?=BBIN::CH ?>',gameId);
+    });
+}
+
+//nt-游戏搜索
+function nt_search(){
+    var b = [];
+    var k = $("#nt-keyword").val();
+    if(k=="")return;
+    $(".games-nt-search").show().click();
+    $("#nt-search-result").html('');
+    $(".nt-item .game-name").each(function(){
+        if($(this).html().indexOf(k)!=-1){
+            $("#nt-search-result").append('<li>'+$(this).parent().html()+'</li>');
+        }
+    });
+    $('#nt-search-result li').bind('click',function(){
+       var gameId = $(this).find("em").attr("class").replace('nt_','');
+       load_nt(<?= $nt_status?>,gameId);
+    });
+}
 </script>
 
 <!-- PT电游客户端下载 -->
